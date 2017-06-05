a22181c974611866431a1012980dbcf9ebcac5d3
Darcy70 <darcy0721@gmail.com>
2017-06-05 14:57:32 +0800

    finished the Ajax first simplest part

--- ajax-posting-app/app/controllers/posts_controller.rb
+++ ajax-posting-app/app/controllers/posts_controller.rb
@@ -29,7 +29,7 @@ class PostsController < ApplicationController
       Like.create(:user => current_user, :post => @post )
     end
 
-    redirect_to posts_path
+    # redirect_to posts_path
 
   end
 
@@ -39,7 +39,8 @@ class PostsController < ApplicationController
     like = @post.find_like(current_user)
     like.destroy
 
-    redirect_to posts_path
+    # redirect_to posts_path
+    render "like"  # if we don't specify the file , Rails will automatically look for the js file named unlike.js.erb
   end
 
 
